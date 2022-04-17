8206e873bf2f1046f64e0949c69096ec09d07902
jtsang4 <wtzeng1@gmail.com>
2022-04-18 01:58:39 +0800

    feat: add stylelint-config-css-modules and no-ignored-properties plugin

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 module.exports = {
-  extends: 'stylelint-config-standard-scss',
+  extends: [
+     'stylelint-config-standard-scss',
+     'stylelint-config-css-modules'
+  ],
+  plugins: ['stylelint-declaration-block-no-ignored-properties'],
   overrides: [
     {
       files: ['**/*.scss'],
       customSyntax: 'postcss-scss',
     },
   ],
+  ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.tsx', '**/*.ts'],
   rules: {
     'selector-class-pattern': null,
   },
