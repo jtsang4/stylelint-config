24416188ca8ba5bac5667f8aae4db3f297c1991d
jtsang4 <wtzeng1@gmail.com>
2022-05-07 15:08:12 +0800

    feat: add recommended and standard stylelint config and support .less file

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 module.exports = {
   extends: [
-     'stylelint-config-standard-scss',
-     'stylelint-config-css-modules'
+    'stylelint-config-recommended',
+    'stylelint-config-standard',
+    'stylelint-config-standard-scss',
+    'stylelint-config-css-modules',
   ],
   plugins: ['stylelint-declaration-block-no-ignored-properties'],
   overrides: [
     {
       files: ['**/*.scss'],
       customSyntax: 'postcss-scss',
+    },
+    {
+      files: ['**/*.less'],
+      customSyntax: 'postcss-less',
     },
   ],
   ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.tsx', '**/*.ts'],
